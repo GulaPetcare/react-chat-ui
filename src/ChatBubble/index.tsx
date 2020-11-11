5bd6b822dfb9e23e610addb5aaa275b585904da1
Yetkin Ergun <yetkin@petsapp.com>
2020-11-11 15:31:51 +0000

    set link color to blue for other side

--- src/ChatBubble/index.tsx
+++ src/ChatBubble/index.tsx
@@ -55,7 +55,8 @@ const MessageUI = styled.p<{ me: boolean }>`
   font-weight: normal;
   margin: 0;
   a {
-    color: ${(props) => props.me ? props.theme.bubbles.mineColor : props.theme.bubbles.theirColor}
+    color: ${(props) =>
+      props.me ? props.theme.bubbles.mineColor : "#0000EE"};
   }
 `;
 
